efc52c92c4beb3831dfcb1531a8d1faf0fc1ba88
paradox <101551548+Sayrayt@users.noreply.github.com>
2024-06-07 03:00:56 +0400

    Update app.js
    fsdfds

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(new Router({ mediator }));
 
-
+//fsdfsdfsdf
 
 function initCB() {
     mediator.call(MEDIATOR.EVENTS.ON_DATABASE_INIT, '');
